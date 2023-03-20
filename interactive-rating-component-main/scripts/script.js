bc8a7b2a6ee8847b92ba902b82f46867a78032c5
Lucas-Rept <borgeslucas19000@gmail.com>
2023-03-20 10:39:38 -0300

    adding rating interactive component challenge

---
+++ interactive-rating-component-main/scripts/script.js
@@ -0,0 +1,30 @@
+let options = document.getElementsByClassName("option");
+let score = undefined;
+let button = document.getElementById("submitButton");
+let scoreSpan = document.getElementById("scoreSpan");
+
+
+for(option of options){
+    option.addEventListener("click", select);
+}
+
+button.addEventListener("click", submit);
+
+function select(event){
+    let target = event.target;
+    for(option of options){
+        option.classList.remove("selected");
+    }
+    target.classList.add("selected");
+    score = target.innerHTML;
+    scoreSpan.innerText = score;
+}
+
+function submit(){
+    let currentSection = document.getElementById("optionsSection");
+    let endSection =  document.getElementById("thanksSection");
+    if(score != undefined){
+        currentSection.style.display = "none";
+        endSection.style.display = "flex";
+    }
+}
